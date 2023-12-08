234a3868709a680c2e66b6f7801bf7ce7efdea51
stefan-apr <32446183+stefan-apr@users.noreply.github.com>
2023-12-07 23:53:01 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vite-deploy-demo/',
   server: {
     host: '0.0.0.0',
   }
